24f7450c96e37732616a30ea62c2af531264d5e2
Armaan Hirani <hiraniarmaan@icloud.com>
2024-04-28 13:35:08 -0500

    inital commit

---
+++ a5.py
@@ -0,0 +1,39 @@
+"""
+Name:
+UTEID:
+replace <NAME> with your name and delete this line.
+
+On my honor, <NAME>, this programming assignment is my own work
+and I have not provided this code to any other student.
+
+Complete the following:
+
+0. What is your email in case I have issues while trying to install, run, and
+use your program.
+
+1. What is the purpose of your program?
+ 
+2. List the major features of your program:
+
+3. What 3rd party modules must be installed for the program to work?
+   (Must be clear and explicit here or we won't be able to test your program.)
+   
+   If it is required to install 3rd party modules include the EXACT pip command.
+
+4. List the things your learned while doing this program. Python features,
+   techniques, third party modules, etc.
+
+5. What was the most difficult thing you had to overcome or learn
+   to get this program to work?
+   
+6. What features would you add next?
+
+"""""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
